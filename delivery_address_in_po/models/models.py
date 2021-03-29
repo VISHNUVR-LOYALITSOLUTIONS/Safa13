9c50f8b1e7e436fe6ada1dac368cec095c731618
Fahadrm <55850573+Fahadrm@users.noreply.github.com>
2021-03-29 10:21:33 +0530

    Add files via upload

--- delivery_address_in_po/models/models.py
+++ delivery_address_in_po/models/models.py
@@ -6,6 +6,6 @@ from odoo import models, fields, api
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    delivery_address_id = fields.Many2one('res.partner', string='Delivery Address', required=True,
+    delivery_address_id = fields.Many2one('res.partner', string='Delivery Address',
                                           domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",)
 
